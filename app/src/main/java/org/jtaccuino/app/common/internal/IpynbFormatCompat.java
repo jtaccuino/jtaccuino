8d764f8a83aa2b9525ae0be106f41dc5a5a97e2b
Martin Klähn <mklaehn@users.noreply.github.com>
2025-05-07 22:27:32 +0200

    Fixing errorprone warning @SuppressWarnings(ArrayRecordComponent) of … (#59)
    * Added NotebookPersistenceTest to ensure the example notebooks can be read by all IpynbFormatOperations
    
    * Fixed parse failure of IpynbFormatCompat and fixed errorprone issue concerning ArrayRecordComponent

--- app/src/main/java/org/jtaccuino/app/common/internal/IpynbFormatCompat.java
+++ app/src/main/java/org/jtaccuino/app/common/internal/IpynbFormatCompat.java
@@ -15,6 +15,7 @@
  */
 package org.jtaccuino.app.common.internal;
 
+import jakarta.json.JsonString;
 import jakarta.json.bind.annotation.JsonbTypeDeserializer;
 import jakarta.json.bind.serializer.DeserializationContext;
 import jakarta.json.bind.serializer.JsonbDeserializer;
@@ -30,12 +31,10 @@ import org.jtaccuino.core.ui.api.CellData;
 
 public record IpynbFormatCompat(Map<String, Object> metadata, int nbformat, int nbformat_minor, List<Cell> cells) implements IpynbFormatOperations {
 
-    @SuppressWarnings("ArrayRecordComponent") // TODO: https://errorprone.info/bugpattern/ArrayRecordComponent What to do?
-    public static record CodeCell(String id, String cell_type, Map<String, Object> metadata, String[] source, List<Output> outputs, int execution_count) implements Cell {
+    public static record CodeCell(String id, String cell_type, Map<String, Object> metadata, List<String> source, List<Output> outputs, int execution_count) implements Cell {
     }
 
-    @SuppressWarnings("ArrayRecordComponent") // TODO: https://errorprone.info/bugpattern/ArrayRecordComponent What to do?
-    public static record MarkdownCell(String id, String cell_type, Map<String, Object> metadata, String[] source) implements Cell {
+    public static record MarkdownCell(String id, String cell_type, Map<String, Object> metadata, List<String> source) implements Cell {
     }
 
     public static record Output(String output_type, Map<String, String> data, Map<String, Object> metadata) {
@@ -50,7 +49,7 @@ public record IpynbFormatCompat(Map<String, Object> metadata, int nbformat, int 
 
         public String id();
 
-        public String[] source();
+        public List<String> source();
     }
 
     public static class CellDeserializer implements JsonbDeserializer<Cell> {
@@ -59,11 +58,25 @@ public record IpynbFormatCompat(Map<String, Object> metadata, int nbformat, int 
         public Cell deserialize(JsonParser parser, DeserializationContext ctx, Type rtType) {
             var o = parser.getObject();
             var type = o.getString("cell_type");
+            var id = o.getString("id");
+            var source = o.getJsonArray("source").stream().map(JsonString.class::cast).map(js -> js.getString()).toArray(String[]::new);
             return switch (type) {
-                case "code" ->
-                    ctx.deserialize(CodeCell.class, parser);
+                case "code" -> {
+                    var outputs = o.getJsonArray("outputs").stream()
+                            .map(v -> v.asJsonObject())
+                            .map(ov
+                                    -> new Output(
+                                    ov.getString("output_type"),
+                                    ov.getJsonObject("data")
+                                            .entrySet()
+                                            .stream()
+                                            .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue().toString())),
+                                    Map.of()))
+                            .toList();
+                    yield new CodeCell(id, type, Map.of(), List.of(source), outputs, 0);
+                }
                 case "markdown" ->
-                    ctx.deserialize(MarkdownCell.class, parser);
+                    new MarkdownCell(id, type, Map.of(), List.of(source));
                 default ->
                     throw new IllegalStateException("Unsupported cell type found: " + type);
             };
@@ -82,7 +95,7 @@ public record IpynbFormatCompat(Map<String, Object> metadata, int nbformat, int 
             case CodeCell c ->
                 CellData.of(
                 CellData.Type.of(c.cell_type()),
-                Arrays.stream(c.source()).collect(Collectors.joining()),
+                String.join("\n", c.source()),
                 c.id() == null ? UUID.randomUUID() : UUID.fromString(c.id()),
                 c.outputs() != null ? c.outputs().stream().map(o
                 -> CellData.OutputData.of(
@@ -93,7 +106,7 @@ public record IpynbFormatCompat(Map<String, Object> metadata, int nbformat, int 
             case MarkdownCell m ->
                 CellData.of(
                 CellData.Type.of(m.cell_type()),
-                Arrays.stream(m.source()).collect(Collectors.joining()),
+                String.join("\n", m.source()),
                 m.id() == null ? UUID.randomUUID() : UUID.fromString(m.id())
                 );
         };
