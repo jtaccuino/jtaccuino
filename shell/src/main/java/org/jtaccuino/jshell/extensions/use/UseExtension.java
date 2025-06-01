48e69a0c68b394095353b6fdddf186af286973e7
Sven Reimers <sven.reimers@gmail.com>
2025-06-01 23:45:10 +0200

    Provide enum as alternative to String API for activating extensions. Fixes #94

--- shell/src/main/java/org/jtaccuino/jshell/extensions/use/UseExtension.java
+++ shell/src/main/java/org/jtaccuino/jshell/extensions/use/UseExtension.java
@@ -15,11 +15,18 @@
  */
 package org.jtaccuino.jshell.extensions.use;
 
+import java.util.Locale;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.jtaccuino.jshell.ReactiveJShell;
 import org.jtaccuino.jshell.extensions.JShellExtension;
 
 public class UseExtension implements JShellExtension {
+
+    public static interface OnDemandExtension {
+
+        public Class<? extends JShellExtension> getExtensionClass();
+    }
 
     private final ReactiveJShell rjs;
 
@@ -32,21 +39,78 @@ public class UseExtension implements JShellExtension {
         return Optional.of("useExtension");
     }
 
+    private String toSnakeCaseEnumName(String extensionShortName) {
+        if (extensionShortName == null) {
+            return null;
+        }
+        if (extensionShortName.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder snakeCaseBuilder = new StringBuilder();
+        int n = extensionShortName.length();
+
+        for (int i = 0; i < n; i++) {
+            char currentChar = extensionShortName.charAt(i);
+
+            // If it's an uppercase character
+            if (Character.isUpperCase(currentChar)) {
+                // Check if it's NOT the first character
+                // AND if the previous character was lowercase (e.g., "myVariable" -> "my_variable")
+                // OR if it's part of a sequence of uppercase characters where
+                //    the next character is lowercase (e.g., "HTTPRequest" -> "http_request")
+                if (i > 0
+                        && (Character.isLowerCase(extensionShortName.charAt(i - 1))
+                        || (i + 1 < n && Character.isLowerCase(extensionShortName.charAt(i + 1))))) {
+                    snakeCaseBuilder.append('_');
+                }
+                snakeCaseBuilder.append(currentChar);
+            } else {
+                // If it's a lowercase character, just append it
+                snakeCaseBuilder.append(currentChar);
+            }
+        }
+
+        return snakeCaseBuilder.toString().toUpperCase(Locale.ENGLISH);
+    }
+
     @Override
     public Optional<String> initCodeSnippet() {
-        return Optional.of("""
+        String enums = JShellExtension.Mode.ON_DEMAND.getExtensionClasses()
+                .stream()
+                .peek(System.out::println)
+                .map(e -> toSnakeCaseEnumName(e.getSimpleName()) + "(" + e.getName() + ".class)")
+                .collect(Collectors.joining(",\n"));
+
+        return Optional.of("enum JTExtension implements org.jtaccuino.jshell.extensions.use.UseExtension.OnDemandExtension {\n" + enums + ";\n"
+                + """
+                              final private Class<? extends org.jtaccuino.jshell.extensions.JShellExtension> extensionClass;
+
+                              public Class<? extends org.jtaccuino.jshell.extensions.JShellExtension> getExtensionClass() {
+                                return extensionClass;
+                              }
+
+                              JTExtension(Class<? extends org.jtaccuino.jshell.extensions.JShellExtension> exClass) {
+                                this.extensionClass = exClass;
+                              }
+                           }
                            public void use(String extensionShortName) {
                              useExtension.use(extensionShortName);
+                           }
+                           public void use(org.jtaccuino.jshell.extensions.use.UseExtension.OnDemandExtension extensionIdentifier) {
+                             useExtension.use(extensionIdentifier);
                            }
                            """);
     }
 
     public void use(String extensionShortName) {
-        JShellExtension.Mode.ON_DEMAND.getExtensionFactories()
-                .stream()
-                .filter(f -> f.getClass().getName().contains(extensionShortName))
-                .findFirst()
-                .ifPresent(f -> rjs.activateExtension(f));
+        JShellExtension.Mode.ON_DEMAND.getFactoryForExtension(extensionShortName)
+                .ifPresent(rjs::activateExtension);
+    }
+
+    public void use(OnDemandExtension extension) {
+        JShellExtension.Mode.ON_DEMAND.getFactoryForExtension(extension.getExtensionClass())
+                .ifPresent(rjs::activateExtension);
     }
 
     @Descriptor(mode = Mode.SYSTEM, type = UseExtension.class)
